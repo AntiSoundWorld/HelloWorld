9762f42fe60fd3e35458b0d6828d712fa0dc1450
AntiSoundWorld <69455703+AntiSoundWorld@users.noreply.github.com>
2020-08-10 13:33:10 +0300

    Edit Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,2 @@
-C:\Users\Poni117>E:
-E:\>cd E:\Dima\C\helloWorld
-E:\Dima\C\HelloWorld>gcc HelloWorld.c -o HelloWorld
-E:\Dima\C\HelloWorld>HelloWorld.exe
-HelloWorld!!!!!!!!!!!!
+gcc HelloWorld.c -o HelloWorld
+HelloWorld.exe
